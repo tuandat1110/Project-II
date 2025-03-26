3a87bed343da80445a9cae3db8768ae9fb26802c
Nguyen Tuan Dat <nguyentuandat1110@gmail.com>
2025-03-26 23:05:27 +0700

    hoan thanh giao dien setting

--- app/src/main/java/com/example/projectii/profile.kt
+++ app/src/main/java/com/example/projectii/profile.kt
@@ -1,10 +1,12 @@
 package com.example.projectii
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +22,8 @@ class profile : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var logout_btn:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +41,15 @@ class profile : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        logout_btn = view.findViewById(R.id.logout_btn)
+        logout_btn.setOnClickListener {
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+        companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
